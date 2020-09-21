da0f60749da582a836067e70318ff4aff381453c
adrianbparra <adrianplane12@gmail.com>
2020-09-21 16:39:43 -0700

    first website creation commit

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Footer = () =>(
+    <footer>
+        <div>
+            <img alt="ap logo"/>
+            <p>I am a Full Stack Web Developer with experience in many languages and library's. I have created many Front end and Back end websites that are built with best practices.</p>
+        </div>
+        <div>
+            <p>Created by Adrian Parra, © {new Date().getFullYear()}</p>
+        </div>
+    </footer>
+)
+
+
+export default Footer

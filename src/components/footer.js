4082f23f392213267037f67d1e6f34fdc00999fe
adrianbparra <adrianplane12@gmail.com>
2020-09-22 23:05:52 -0700

    added basic frame for components

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,49 @@
 import React from "react"
+import {Link} from "gatsby"
+
+import { FaLinkedin,FaTwitterSquare,FaGithubSquare } from 'react-icons/fa';
+import APLogo from "../images/AP-Logo.png"
 
 const Footer = () =>(
     <footer>
         <div>
-            <img alt="ap logo"/>
-            <p>I am a Full Stack Web Developer with experience in many languages and library's. I have created many Front end and Back end websites that are built with best practices.</p>
+            <div className="details">
+                <img src={APLogo} alt="ap logo"/>
+                <p>I am a Full Stack Web Developer with experience in many <Link to="/about">languages and library's</Link>. I have created many Front end and Back end websites that are built with best practices.</p>
+            </div>
+            <div className="social">
+                <a href="https://github.com/adrianbparra" target="_blank" rel="noreferrer">
+                    <FaGithubSquare/>
+                    <p>Github</p>
+                </a>
+                <a href="https://www.linkedin.com/in/adrian-parra/" target="_blank" rel="noreferrer">
+                    <FaLinkedin/>
+                    <p>Linkedin</p>
+                </a>
+                <a href="https://twitter.com/Adrian_B_Parra" target="_blank" rel="noreferrer">
+                    <FaTwitterSquare/>
+                    <p>Twitter</p>
+                </a>
+            </div>
+            <dl className="pages">
+                <div>
+                    <dt><Link to="/portfolio">Portfolio</Link></dt>
+                    <dl>Take a look at my projects</dl>
+                </div>
+                <div>
+                    <dt><Link to="/about">About</Link></dt>
+                    <dl>Learn all about me and what I do</dl>
+                </div>
+                <div>
+                    <dt><Link to="/contact">Contact</Link></dt>
+                    <dl>Send me a messsage</dl>
+                </div>
+            </dl>
+
         </div>
-        <div>
-            <p>Created by Adrian Parra, © {new Date().getFullYear()}</p>
-        </div>
+        <small>
+            <p>Created by <Link to="/">Adrian Parra</Link>, © {new Date().getFullYear()}</p>
+        </small>
     </footer>
 )
 
--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,45 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-  >
-    
-    <h1>
-      <Link
-        to="/"
-      >
-        {siteTitle}
-      </Link>
-    </h1>
 
-  
-    <Link to="/portfolio">Portfolio</Link>
+import APLogo from "../images/AP-Logo.png"
 
-    <Link to="/about">About</Link>
-  </header>
-)
+
+const Header = ({ siteTitle }) => {
+
+
+  return (
+    <header style={{paddingTop: "1rem", paddingBottom: "1rem"}}>
+      <div>
+        <Link
+          to="/"
+        >
+          <img src={APLogo} alt="AP-Logo"/>
+      
+          <h1>{siteTitle}</h1>
+        </Link>
+      </div>
+      
+      <nav>
+        <ul>
+          <li>
+          <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -28,5 +49,7 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: ``,
 }
+
+
 
 export default Header

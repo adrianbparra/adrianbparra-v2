a0fd62eb1d1e0084342310596984763f6e6844f7
adrianbparra <adrianplane12@gmail.com>
2022-05-02 19:20:04 -0700

    added about and contact info
    Added about page and a way to contact me using getform.io

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,77 @@
 import React from "react"
+import { Container } from "react-bootstrap"
+import { Link } from 'gatsby';
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const About = () => (
-    <Layout>
+    <Layout page="about">
         <SEO title="About"/>
-        <h1>About</h1>
+
+        <Container fluid className="header">
+            <h1>About Adrian</h1>
+        </Container>
+
+        <Container className="info">
+
+            <p>
+                I am a Full Stack Web Developer creating website that make a positive change.
+                I create website that are reliable, easy to use, and with best practices.
+                I love to learn new technology to achieve my dreams and even yours to complete your project.
+            </p>
+
+            <p>
+                I love to create websites that I am proud of and that successfully complete the requirements.
+                I work with your specific business requirements to complete the project to your needs.
+            </p>
+
+            <p>
+                My main experiance is in Front End development but a passion for Back End data management as well. 
+                With the love for Full Stack Web Development I can create applicatons that not only look appealing but handle data for your business.
+                There is so many areas in Development it is always best to know parts of all to better understand client needs and requirements.
+            </p>
+
+            <p>
+                I take pride in completing client needs and offer solutions that resolve problems they encounter. 
+                I will work until the project is 100% completed and operational.
+            </p>
+
+            <h2>
+                Front End
+            </h2>
+
+            <p>
+                I use HTML, CSS, JavaScript and C# to create responsive websites that provide best practice and clean code.
+            </p>
+
+            <p>
+                I am also familiar with other tools and frameworks that fall under the categories above.
+                If you have questions about my knowledge <Link to={"/contact"} className="contact-link">Contact Me</Link> to find out how I can help you with your website.
+            </p>
+
+            <h2>
+                Back End
+            </h2>
+
+            <p>
+                I use JavaScript, C#, SQL, NoSQL, to handle your important data with care.
+            </p>
+
+            <p>
+                I can create different types of servers to handle your important data that is easily accessible and safe.
+                I use technology that better suites your business needs.
+                I am capable or learning new technology if it comes to making your site even better.
+            </p>
+
+        </Container>
+
+        <Container className="button-container">
+            <Link to={"/contact"} className="contact-me-button">
+                Contact Me
+            </Link>
+        </Container>
+
     </Layout>
 )
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,46 @@
 import React from "react"
+import { Container, Form, Button } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Contact = () =>(
-    <Layout>
+    <Layout page={"contact"}>
         <SEO title="Contact"/>
-        <h1>Contact</h1>
+
+        <Container fluid className="header">
+            <h1>Get in Touch</h1>
+            <p>Send me a message or details of the project you would like me to be a part of.</p>
+        </Container>
+
+        <Container fluid className="form">
+            <Form method="POST" action="https://getform.io/f/ecf09f8c-aaf4-473a-a0aa-0b7b2d4eecbe">
+                <Form.Group className="fields">
+                    <Form.Label>
+                        Your Name
+                        <span className="req">*</span>
+                    </Form.Label>
+                    <Form.Control required type="text" name="name" placeholder="Name"/>
+                    
+                    <Form.Label>
+                        Your Email
+                        <span className="req">*</span>
+                    </Form.Label>
+                    <Form.Control required type="email" name="email" placeholder="name@example.com" />
+
+                    <Form.Label>Your Website</Form.Label>
+                    <Form.Control type="url" placeholder="adrianbparra.com"/>
+
+                    <Form.Label>
+                        Your Message
+                        <span className="req">*</span>
+                    </Form.Label>
+                    <Form.Control required as="textarea" name="message" rows={4} />
+                </Form.Group>
+
+                <Button type="submit"> Submit </Button>
+            </Form>
+        </Container>
+
     </Layout>
 )
 

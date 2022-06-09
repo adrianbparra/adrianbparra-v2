de8bcfe6eece07403cf49efb538c40d2694cbff3
adrianbparra <adrianplane12@gmail.com>
2022-06-08 19:52:20 -0700

    Added recaptcha
    Added recaptcha on contact email submit to stop bots from spamming email

---
+++ src/components/contactModal.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+
+const ContactModal = (props) => {
+
+    const {show, onHide, msg} = props;
+
+    return (
+      <Modal
+        show = {show}
+        size="lg"
+        aria-labelledby="contact-email-modal"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contact-email-modal">
+            Contact Email
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+           {msg}
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+
+  }
+
+export default ContactModal
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,47 +1,145 @@
-import React from "react"
-import { Container, Form, Button } from "react-bootstrap"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React,{useState, useRef} from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import ReCAPTCHA from "react-google-recaptcha";
 
-const Contact = () =>(
-    <Layout page={"contact"}>
-        <SEO title="Contact"/>
+import ContactModal from "../components/contactModal";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-        <Container fluid className="header">
-            <h1>Get in Touch</h1>
-            <p>Send me a message or details of the project you would like me to be a part of.</p>
-        </Container>
+const RECAPTCHA_KEY = "6LcaueofAAAAADjjuMh6uJCAXE5A9MWYvWdLANNf"
 
-        <Container fluid className="form">
-            <Form method="POST" action="https://getform.io/f/ecf09f8c-aaf4-473a-a0aa-0b7b2d4eecbe">
-                <Form.Group className="fields">
-                    <Form.Label>
-                        Your Name
-                        <span className="req">*</span>
-                    </Form.Label>
-                    <Form.Control required type="text" name="name" placeholder="Name"/>
+const Contact = () =>{
+    const [serverState, setServerState] = useState({
+        submitting: false,
+        showModal:  false,
+        msg: "",
+        ok:null
+    });
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const recaptchaRef = useRef(null);
+    
+
+    const handleServerResponse = (ok, msg, form) => {
+        setButtonDisabled(true);
+        recaptchaRef.current.reset();
+        setServerState({
+          submitting: false,
+          showModal: true,
+          ok:ok,
+          msg:msg        
+        });
+        if (ok) {
+          form.reset();
+        }
+
+        console.log(ok,msg,form)
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const form = e.target;
+        setServerState({ submitting: true });
+
+        const formData = new FormData(form);
+
+        const recaptchaValue = recaptchaRef.current.getValue();
+
+        if (recaptchaValue){
+
+            formData.append(
+                "g-recaptcha-response",
+                recaptchaValue
+            )
+            
+            fetch("https://getform.io/f/ecf09f8c-aaf4-473a-a0aa-0b7b2d4eecbe", {
+                method: "POST",
+                body: formData
+            })
+            .then((r)=> {
+                if(!r.ok){
+                    handleServerResponse(false, "Sorry please try again later.", form);
+                }
+                else{
                     
-                    <Form.Label>
-                        Your Email
-                        <span className="req">*</span>
-                    </Form.Label>
-                    <Form.Control required type="email" name="email" placeholder="name@example.com" />
+                    handleServerResponse(true, "Thank you for your email, I look foward to working with you.", form);
+                }
+            })
+            .catch((r)=>{
+                handleServerResponse(false, "Sorry, there was an error please try again later.", form);
+            })
+        }
 
-                    <Form.Label>Your Website</Form.Label>
-                    <Form.Control type="url" placeholder="adrianbparra.com"/>
 
-                    <Form.Label>
-                        Your Message
-                        <span className="req">*</span>
-                    </Form.Label>
-                    <Form.Control required as="textarea" name="message" rows={4} />
-                </Form.Group>
+    }
 
-                <Button type="submit"> Submit </Button>
-            </Form>
-        </Container>
+    const verifyCallback =  res => {
+        if (res){
+            setButtonDisabled(false);
+        }
+        else {
+            setButtonDisabled(true)
+        }
+                                                                                       
+    }
 
-    </Layout>
-)
+    return (
+
+        <Layout page={"contact"}>
+            <SEO title="Contact"/>
+
+            <Container fluid className="header">
+                <h1>Get in Touch</h1>
+                <p>Send me a message or details of the project you would like me to be a part of.</p>
+            </Container>
+
+            <Container fluid className="form">
+                <Form
+                    id="contact"
+                    onSubmit={handleSubmit}
+                >
+                    <Form.Group className="fields">
+                        <Form.Label>
+                            Your Name
+                            <span className="req">*</span>
+                        </Form.Label>
+                        <Form.Control required type="text" name="name" placeholder="Name"/>
+                        
+                        <Form.Label>
+                            Your Email
+                            <span className="req">*</span>
+                        </Form.Label>
+                        <Form.Control required type="email" name="email" placeholder="name@example.com"/>
+
+                        <Form.Label>Your Website</Form.Label>
+                        <Form.Control type="text" name="website" placeholder="domain.com"/>
+
+                        <Form.Label>
+                            Your Message
+                            <span className="req">*</span>
+                        </Form.Label>
+                        <Form.Control required as="textarea" name="message" rows={4}/>
+                        
+                        <ReCAPTCHA
+                            className="g-recaptcha"
+                            ref={recaptchaRef}
+                            sitekey={RECAPTCHA_KEY}
+                            onChange={verifyCallback}
+                            onExpired= {()=>{setButtonDisabled(true)}}
+                        />
+                    </Form.Group>
+
+                    <Button type="submit" disabled={buttonDisabled | serverState.submitting}> Submit </Button>
+                </Form>
+            </Container>
+
+            <ContactModal
+                show={serverState.showModal}
+                onHide={()=>setServerState({showModal:false})}
+                msg = {serverState.msg}
+            />
+
+        </Layout>
+    )
+}
 
 export default Contact
